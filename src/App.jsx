fbc7ed984849f2ccacd64c43874d8b10b311d6f1
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-08-31 22:04:35 +0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App () {
   return (
     <BrowserRouter>
-     <Routes>
-        <Route path="/" element={<Home />} />
-     </Routes>
+      <Routes>
+          <Route path="/" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   )
 }
