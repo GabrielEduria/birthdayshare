8aeb077c7a77e68678750d21be74107e3e1d4a2e
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-09-01 08:01:49 +0800

    push Home Jsx components

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,16 @@
 
 
-
-export default function Home () {
+function Home () {
     return (
-        <>
-        </>
+       <section className="w-full h-screen bg-amber-400 flex">
+         <div className="h-full w-1/2 border-2 left-0">
+            
+         </div>
+         <div className="h-full w-1/2 border-2">
+
+         </div>
+       </section>
     )
 }
+
+export default Home;
