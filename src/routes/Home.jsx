c6041454e4c1bf26aad54bb34cd9061797877cff
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-10-18 17:52:14 +0800

    fixed .env route

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,10 +4,10 @@ function Home () {
     return (
        <section className="w-full h-screen  flex">
          <div className="h-full w-1/2 border-2 left-0">
-            
+            Hello
          </div>
          <div className="h-full w-1/2 border-2">
-
+           DMT
          </div>
        </section>
     )
