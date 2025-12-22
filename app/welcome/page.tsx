a1f570eba410b57628046ece56ef706833d965d5
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-12-22 23:41:20 +0800

    update

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -4,7 +4,7 @@ import Button from "../components/button/Button";
 
 export default function Welcome() {
 return (
-  <div className="relative min-h-screen w-full overflow-hidden bg-linear-to-r from-rose-200 to-yellow-100  dark:bg-gray-950">
+  <div className="relative h-screen w-full overflow-hidden bg-linear-to-r from-rose-200 to-yellow-100  dark:bg-gray-950">
 
   {/*BG Tailwind Snippet*/}
     <div className="absolute -inset-[10%] overflow-hidden">
@@ -23,16 +23,15 @@ return (
 
     <div className="absolute inset-0 h-full backdrop-blur-[6px]" />
 
-
    {/* Home Display */}
-    <div className="h-full w-full flex justify-center items-center">
+    <div className="h-full w-full flex justify-center items-center ">
 
-      <div className="relative z-30 text-center px-6 border border-black">
+      <div className="relative h-1/2 z-30 text-center px-6">
         <p className="font-roboto inline-block rounded-full shadow-2xl px-4 py-1 text-black backdrop-blur-xl bg-white/20">Birthday Share!</p>
-       <h1 className="mx-auto max-w-5xl font-happy-monkey text-4xl lg:text-6xl px-12 text-center tracking-tight text-black font-black leading-tight">
+       <h1 className="mx-auto max-w-5xl font-happy-monkey text-4xl lg:text-6xl px-12 text-center tracking-tight text-black font-black leading-tight pt-10">
           A shareable birthday canvas url maker made easy to use
         </h1>
-        <h2 className="font-roboto text-lg text-center tracking-tight text-black leading-tight px-12 mt-4">
+        <h2 className="font-roboto text-lg text-center tracking-tight text-black leading-tight px-12 mt-5">
           A quick minute tool, responsive canvas maker, Zero-hassle, No account Needed
         </h2>
         <div className="flex justify-center mt-4">
